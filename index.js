ce59d3242a46d6bf30fed28a08f2e5833586311e
Cristy Lucke <cristy.lucke@gmail.com>
2017-09-30 23:54:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return ("Welcome, " + name + ". You are number " + katzDeliLine.length.toString() + " in line.")
 }
+
+function nowServing() {
+  if katzDeliLine.length > 0 {
+    return katzDeliLine.shift()
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
