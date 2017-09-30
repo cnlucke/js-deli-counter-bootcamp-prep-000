dbc74ba1a07ba7a394143238067be13e015e346e
Cristy Lucke <cristy.lucke@gmail.com>
2017-09-30 23:36:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var katzDeliLine = []
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
+  console.log("Welcome, " + name + ". You are number " katzDeliLine.length.toString() + " in line.")
   return katzDeliLine.length
 }
